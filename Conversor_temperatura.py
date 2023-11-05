97caa46958773c539837c502b3d53c758bedb6f9
Henpai01 <emprendeconmigo2004@gmail.com>
2023-11-04 21:02:17 -0400

    WorkPls

--- Conversor_temperatura.py
+++ Conversor_temperatura.py
@@ -1,27 +1,25 @@
-import tkinter as tk
+import tkinter as tk # noqa
 from tkinter import ttk
-from tkinter import *
+from tkinter import * # noqa
 
-#Creamos la funcion para convertir
+
+# Creamos la funcion para convertir
 def borrar_cajas(caja_temp_cel, caja_temp_kelvin, caja_temp_farenheit):
         caja_temp_cel.delete(0, "")
         caja_temp_kelvin.delete(0, "")
         caja_temp_farenheit.delete(0, "")
 
 
-    
 def create_temperaturas_widget(ventana):
     """
     Crear todos los componentes y asociar las acciones.
     """
 
-    frame = Frame(ventana)
-
-    
+    frame = Frame(ventana) # noqa
 
     # Definimos el texto del boton
 
-    etiqueta_temp_celsius = ttk.Label(frame, text= "Ingresar temperatura en °C")
+    etiqueta_temp_celsius = ttk.Label(frame, text="Ingresar temperatura en °C")
     # Definimos el lugar donde va el boton
 
     etiqueta_temp_celsius.place(x=20, y=25)
@@ -30,31 +28,29 @@ def create_temperaturas_widget(ventana):
 
     caja_temp_cel = ttk.Entry(frame)
 
-    #Definimos donde va el input
+    # Definimos donde va el input
 
-    caja_temp_cel.place(x= 170, y= 25, width= 60)
-
+    caja_temp_cel.place(x=170, y=25, width=60)
 
     # Definimos las etiquetas Kelvin
 
-    etiqueta_temp_kelvin = ttk.Label(frame, text= "Temperatura en K: ")
+    etiqueta_temp_kelvin = ttk.Label(frame, text="Temperatura en K: ")
     etiqueta_temp_kelvin.place(x=20, y=50)
     caja_temp_kelvin = ttk.Entry(frame)
-    caja_temp_kelvin.place(x=170 , y=50, width=60)
+    caja_temp_kelvin.place(x=170, y=50, width=60)
 
     # Definimos los Farenheits
 
     etiqueta_temp_farenheit = ttk.Label(frame, text="Temperatura en F: ")
     etiqueta_temp_farenheit.place(x=20, y=75)
     caja_temp_farenheit = ttk.Entry(frame)
-    caja_temp_farenheit.place(x=170, y= 75, width=60)
-
+    caja_temp_farenheit.place(x=170, y=75, width=60)
 
     # Creamos el boton borrar
-    
-    boton_borrar = ttk.Button(frame, text="Borrar", command=lambda:borrar_cajas(caja_temp_cel, caja_temp_kelvin, caja_temp_farenheit))
+
+    boton_borrar = ttk.Button(frame, text="Borrar", command=lambda: borrar_cajas(caja_temp_cel, caja_temp_kelvin, caja_temp_farenheit)) # noqa
     boton_borrar.place(x=100, y=350)
- 
+
     def convertir():
         temp_celsius = float(caja_temp_cel.get())
         temp_kelvin = temp_celsius + 273.15
@@ -76,19 +72,15 @@ def create_temperaturas_widget(ventana):
         caja_temp_cel.insert(0, temp_celsius)
         caja_temp_kelvin.insert(0, temp_kelvin)
 
-
-    #Definimos el buttom 
-
-    boton_convertir = ttk.Button(frame, text="Convertir", command=lambda: convertir() 
-                                if caja_temp_cel.get() 
-                                else convertir_kelvin() 
-                                if caja_temp_kelvin.get() 
+    # Definimos el buttom
+    boton_convertir = ttk.Button(frame, text="Convertir", command=lambda: convertir() # noqa 
+                                if caja_temp_cel.get()
+                                else convertir_kelvin()
+                                if caja_temp_kelvin.get()
                                 else convertir_fahrenheit())
 
-    
     # Definimos el lugar del boton
 
     boton_convertir.place(x=20, y=350)
-    
 
     return frame
